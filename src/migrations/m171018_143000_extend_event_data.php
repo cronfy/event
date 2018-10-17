75b245192401b8128913468c9c19e7ed8238e46d
Олег <cronfy@gmail.com>
2018-10-17 09:18:09 +0300

    extend data

---
+++ src/migrations/m171018_143000_extend_event_data.php
@@ -0,0 +1,25 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `event`.
+ */
+class m171018_143000_extend_event_data extends Migration
+{
+    /**
+     * @inheritdoc
+     */
+    public function up()
+    {
+        $this->alterColumn('event', 'data', $this->string(1024));
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function down()
+    {
+        return false;
+    }
+}
